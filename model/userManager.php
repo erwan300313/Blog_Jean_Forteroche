8e5d7a2e5104ef5be810d9232a35311a093f6f33
Bridier Erwan <e.bridier@hotmail.com>
2020-09-28 22:42:09 +0200

    code cleaning

--- model/userManager.php
+++ model/userManager.php
@@ -5,14 +5,7 @@ require_once("model/manager.php");
 
 class UserManager extends Manager
 {
-    /* public function getLastUser(){
-        $db = $this->dbConnect();
-        $req = $db->query('SELECT id, pseudo, DATE_FORMAT(date_inscription, \'%d/%m/%Y\') AS date_inscirption FROM users ORDER BY id DESC
-        LIMIT 1');
-        $lastUser = $req->fetch();
-        return $lastUser;
-    } */
-
+    
     public function getLastUser(){
         $sql = 'SELECT id, pseudo, DATE_FORMAT(date_inscription, \'%d/%m/%Y\') AS date_inscirption FROM users ORDER BY id DESC
         LIMIT 1';
@@ -20,26 +13,11 @@ class UserManager extends Manager
         return $getLastUser->fetch();
     }
 
-    /* public function userCheck($pseudo){
-        $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id,id_status,firstName, lastName, pseudo, password, mail, DATE_FORMAT(date_inscription, \'%d/%m/%Y\') AS date_inscription FROM users WHERE pseudo = ? ');
-        $req->execute(array($pseudo));
-        $userCheck = $req->fetch();
-        return $userCheck;
-    } */
-
     public function userCheck($pseudo) {
         $sql = 'SELECT id,id_status,firstName, lastName, pseudo, password, mail, DATE_FORMAT(date_inscription, \'%d/%m/%Y\') AS date_inscription FROM users WHERE pseudo = ? ';
         $userCheck = $this->executerRequete($sql, array($pseudo));
         return $userCheck->fetch(); 
     }
-
-    /* public function addUser($lastName, $firstName, $pseudo, $password, $mail, $id_status)
-    {
-        $db = $this->dbConnect();
-        $comments = $db->prepare('INSERT INTO users(id_status, firstName, lastName, pseudo, password, mail, date_inscription) VALUES(?, ?, ?, ?, ?, ?,NOW())');
-        $affectedLines = $comments->execute(array($id_status, $firstName, $lastName, $pseudo, $password, $mail));
-    } */
 
     public function addUser($lastName, $firstName, $pseudo, $password, $mail, $id_status) {
         $sql = 'INSERT INTO users(id_status, firstName, lastName, pseudo, password, mail, date_inscription) VALUES(?, ?, ?, ?, ?, ?,NOW())';
